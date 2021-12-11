6ec437098c9528d78c7a4f10441c2c1336168c09
Om <92863779+MasterBaiter123@users.noreply.github.com>
2021-12-11 07:24:11 +0530

    Fix datetime usage in members command

--- src/cogs/utility.py
+++ src/cogs/utility.py
@@ -517,7 +517,7 @@ class Utility(commands.Cog, name="utilities", description="Useful stuff"):
         embed.set_author(name="Member Count", icon_url=self.bot.user.avatar.url)
         embed.add_field(name="Current Member Count:", value=ctx.guild.member_count)
         embed.set_footer(text=ctx.guild, icon_url=ctx.guild.icon.url)
-        embed.timestamp = datetime.datetime.utcnow()
+        embed.timestamp = datetime.utcnow()
 
         await ctx.send(embed=embed)
 
